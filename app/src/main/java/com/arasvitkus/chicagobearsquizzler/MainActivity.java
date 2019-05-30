e6630636051f6ee5ed53caee028bc33048c739f2
arasmv <amvitkus@gmail.com>
2019-05-30 13:55:59 -0400

    Added quiz question, up to 28.
    Signed-off-by: arasmv <amvitkus@gmail.com>

--- app/src/main/java/com/arasvitkus/chicagobearsquizzler/MainActivity.java
+++ app/src/main/java/com/arasvitkus/chicagobearsquizzler/MainActivity.java
@@ -50,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
             new TrueFalse(R.string.question_24, true),
             new TrueFalse(R.string.question_25, true),
             new TrueFalse(R.string.question_26, false),
-            new TrueFalse(R.string.question_27, false)
+            new TrueFalse(R.string.question_27, false),
+            new TrueFalse(R.string.question_28, true)
     };
 
     //Progress bar constant, had to move here for the code to work properly
