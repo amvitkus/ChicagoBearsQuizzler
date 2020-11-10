adbf7a956b72e600380c15c0e55dec1d6ad541bc
Aras <amvitkus@gmail.com>
2020-11-10 14:27:45 -0500

    Updated project from Java to Kotlin.

---
+++ app/src/main/java/com/arasvitkus/chicagobearsquizzler/TrueFalse.kt
@@ -0,0 +1,5 @@
+package com.arasvitkus.chicagobearsquizzler
+
+class TrueFalse(//Created getters and setters, currently only use getters
+        //Declare member variables
+        var questionID: Int, var isAnswer: Boolean)
